b41a92ddcef1fab205a3b693fb63cd0956cb63a3
shashank42 <devo.shanky@gmail.com>
2023-03-05 00:01:24 -0700

    Update contract address

--- frontend/src/utils/provider.js
+++ frontend/src/utils/provider.js
@@ -5,14 +5,18 @@ import { ethers } from "ethers";
 export const providers = async ( chain ) =>
 {
 
-
     // return providerUrls.get( chain ).wsUrl
     //     ? new WebSocketProvider( providerUrls.get( chain ).wsUrl )
     //     : new ethers.providers.JsonRpcProvider( providerUrls.get( chain ).url );
 
     const wssUrl = "wss://polygon-mainnet.g.alchemy.com/v2/_tn9X7pFnXwYXYi8Q33gQjRg_B3Dey_4";
 
-    return new ethers.providers.WebSocketProvider(wssUrl);
+    const httpUrl = "https://goerli.base.org"
+
+
+    // return new ethers.providers.WebSocketProvider(wssUrl);
+
+    return new ethers.providers.JsonRpcProvider( httpUrl );
 
 
 };
